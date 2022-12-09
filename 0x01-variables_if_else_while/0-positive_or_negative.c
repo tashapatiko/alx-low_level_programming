26ae7344f2c7bcd7c5e13c2951a8241c5e145d08
tashapatiko <patikoleungo@gmail.com>
2022-12-09 13:51:56 -0800

    task 0 completed

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,13 +7,15 @@
  *
  * Return: Always 0 (Success)
  *
- */
+*/
+
 int main(void)
 {      
        	int n;
 
         srand(time(0));
         n = rand() - RAND_MAX / 2;
+
         if (n > 0)
 	{
 	        printf("%d is positive\n", n);
