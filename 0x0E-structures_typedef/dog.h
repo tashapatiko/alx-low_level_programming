e037a045517a789aa9b916ee372db720b84086ac
tashapatiko <patikoleungo@gmail.com>
2023-01-17 13:39:54 -0800

    task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,4 +1,4 @@
-#ifndef MAINH_H
+#ifndef MAIN_H
 #define MAIN_H
 
 /**
@@ -14,7 +14,7 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-}
+};
 
 typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
