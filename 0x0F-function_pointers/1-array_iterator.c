9f3be3890bf39004efe90384c6fbe7f2839f8e69
tashapatiko <patikoleungo@gmail.com>
2023-01-18 16:01:59 -0800

    task 1 completed

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,13 +9,13 @@
  * @action: pointer to function used
  */
 
-void array_iterator(int *array, size_t size, void (*action)(int));
+void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
 	if (!array || !action)
 		return;
-	
+
 	for (i = 0; i < size; i++)
 		action(array[i]);
 }
